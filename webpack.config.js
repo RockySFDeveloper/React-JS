8ebd5bdc4282449c58ec41de034a311dc34f8c70
RockySFDeveloper <hplaptop13092018@gmail.com>
2022-11-28 10:07:05 +0530

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "force-app/main/default/staticresources/reactComponent"),
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "build"),
+    },
+    port: 3070,
+  },
+  module: {
+    // exclude node_modules
+    rules: [
+      {
+        test: /\.(js|jsx)$/,         // <-- added `|jsx` here
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  // pass all js files through Babel
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],    // <-- added `.jsx` here
+  },
+};
